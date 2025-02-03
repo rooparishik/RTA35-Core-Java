fb4935ba799cb7e6adad7b67fa0934219b689786
rooparishik <roopa.bussa@gmail.com>
2025-02-03 18:11:58 -0500

    ControlStatements ForLoop examples

---
+++ src/com/roopa/learning/core/ControlStatements/ForLoopExamples.java
@@ -0,0 +1,52 @@
+package com.roopa.learning.core.ControlStatements;
+
+public class ForLoopExamples
+{
+
+       /*  Syntax of Forloop
+        for(initialization_of_value; condition; Update_the_value){
+        }
+         */
+
+    public static void main(String[] args) {
+     //example1();
+     //example2();
+        //multipleVariableOnForloop();
+        forLoopWithoutInitializationAndUpdate();
+    }
+    private static void  forLoopWithoutInitializationAndUpdate(){
+       int i = 0;
+       for(; i <= 5 ;){
+            System.out.println("i: " + i);
+            i++;
+
+       }
+
+
+    }
+
+    private static void multipleVariableOnForloop(){
+        for(int i = 0, j = 10 ; i <= 10 && j >= 0; i++, j--){
+            System.out.println("i:: " + i + ", j:: " + j);
+        }
+
+    }
+    private  static  void example2(){
+
+
+
+        for (int i = 5; i>=1; i--){
+            System.out.println(" Hello " +  i);
+        }
+    }
+
+
+    private  static  void example1(){
+
+
+
+        for (int counter = 0; counter<=10; counter++){
+            System.out.println(" Hello " +  counter);
+        }
+    }
+}
---
+++ src/com/roopa/learning/core/ControlStatements/MultiDimensionalPrrocessingExamples.java
@@ -0,0 +1,56 @@
+package com.roopa.learning.core.ControlStatements;
+
+public class MultiDimensionalPrrocessingExamples {
+
+    public static void main(String[] args) {
+        //loopInsideLoop();
+        //twoDimensionalArrayProcessing();
+        starPattern();
+
+    }
+
+
+    private static void starPattern(){
+        int rows = 5;
+          for(int i = 1; i <= rows ; i++){
+
+              for(int j = 1; j <= i; j++ ){
+                  System.out.print("* ");
+              }
+              System.out.println();
+          }
+
+
+
+    }
+    private static void twoDimensionalArrayProcessing(){
+         int numbers[][] = {
+                 {1,2,3,67,87,12,16},
+                 {4,5,6,53,17},
+                 {7,8,9,21,20,28},
+         };
+
+         for(int row = 0; row < numbers.length ; row++){
+             for(int column = 0; column < numbers[row].length ; column++){
+
+                 System.out.print(numbers[row][column] + "   ");
+             }
+             System.out.println();
+         }
+
+
+
+
+    }
+
+    private static void loopInsideLoop(){
+
+        for(int i = 0; i <= 5; i++){
+            System.out.println("i:: " + i);
+            for(int j = 0; j <= 5; j++){
+                System.out.println(" j:: " + j);
+            }
+            System.out.println();
+        }
+    }
+}
