ee22f7da93269738b9801840e3d78e2b80c06d05
rooparishik <roopa.bussa@gmail.com>
2025-02-05 18:09:48 -0500

    Control statements and Encapsulation programs

---
+++ src/com/roopa/learning/core/DailyAssignments/EvenAndOddNumbers.java
@@ -0,0 +1,25 @@
+package com.roopa.learning.core.DailyAssignments;
+
+import java.util.Scanner;
+
+public class EvenAndOddNumbers {
+
+    public static void main(String[] args) {
+      /*  System.out.println("Even numbers from 1 to 20 : ");
+
+        for(int i = 1; i <= 20  ; i++) {
+            if (i % 2 == 0) {
+                System.out.print(i + " " );
+            }
+        }*/
+       /* System.out.println("odd numbers from 1 to 20 : ");
+
+        for(int i = 1; i <= 20  ; i++) {
+            if (i % 2 != 0) {
+                System.out.print(i + " " );
+            }
+        }*/
+
+
+    }
+}
---
+++ src/com/roopa/learning/core/oops/Encapsulation/Engine.java
@@ -0,0 +1,9 @@
+package com.roopa.learning.core.oops.Encapsulation;
+
+public class Engine {
+
+    private String touque;
+    private String hp;
+
+    public void start(){}
+}
